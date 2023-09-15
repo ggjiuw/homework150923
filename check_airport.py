59475039f2e9faca05231f4716ae4a8474f8aaae
ggjiuw <herezoom1231@gmail.com>
2023-09-16 02:10:49 +0300

    homework with open()

--- check_airport.py
+++ check_airport.py
@@ -2,5 +2,5 @@
 with open('airport-codes_csv.csv', mode='r', encoding='utf-8') as file:
     for line in file.readlines():
         data = line.split(';')
-        if data[5] == 'UA':
+        if data[5] == 'UA': # in order to change the country, instead of UA, enter the country you need, for example US
             print(data[2])
